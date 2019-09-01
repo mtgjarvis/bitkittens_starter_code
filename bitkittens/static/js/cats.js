5364e4836fe43dd991df61f99dd9ba095a5c8417
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-01 16:45:10 -0400

    completed cat axios request and population of photos

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    summonKitties = document.querySelector('.summon-cats');
+
+    summonKitties.addEventListener('click', () => {
+        axios.get('https://eric-deploy-test.herokuapp.com/cats').then(function(response) {
+            response.data.cats.forEach((cat, index) => {
+                const img = document.createElement('img')
+                img.setAttribute('src', cat.photo)
+                img.setAttribute('alt', `Photo of ${cat.name}`)
+                const container = document.querySelector('#cat'+ (index +1))
+                container.innerHTML = ''
+                container.appendChild(img)
+            });
+        })
+    })    
 });
